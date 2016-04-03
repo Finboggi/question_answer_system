c3fb475feef5ee69da54f50d3beb8ed8946dcaf9
spawny <spawny@yandex.ru>
2016-04-03 22:12:53 +0300

    Добавлен тест на существование нового вопроса во в questions#show

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -34,6 +34,10 @@ RSpec.describe QuestionsController, type: :controller do
       expect(assigns(:question)).to eq question
     end
 
+    it 'assigns a new Answer to @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
     it 'renders :show question view' do
       expect(response).to render_template :show
     end
