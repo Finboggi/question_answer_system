a506b5434c48595972973bec3a31442b504fa2fd
spawny <spawny@yandex.ru>
2016-06-21 10:54:40 +0300

    Создана модель attachments

---
+++ spec/features/questions/add_files_to_question_spec.rb
@@ -0,0 +1,26 @@
+require_relative '../feature_helper'
+
+feature 'add files to question', %q(
+  In order to illustrate my question
+  as question's author
+  I'd like to be able to attach files
+) do
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+
+  background { sign_in(user) }
+
+  scenario 'User adds files when asks question' do
+    visit questions_path
+
+    click_on I18n.t 'questions.new.link'
+
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'Text text'
+    attach_file 'File', 'Gemfile.lock'
+
+    click_on 'Ask'
+  end
+
+  scenario 'User adds files when update question'
+end
